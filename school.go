83fbf794730da67934c85428bf965e43db598bb2
jim.c <jim.c@lgroup.global>
2020-08-19 10:54:18 +0800

    mail

--- school.go
+++ school.go
@@ -1,7 +1,25 @@
 package school
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
-func School() {
-	fmt.Println("this is school")
+func Mail(api string, subject string, title string, content string, targetSlice... string)  {
+	target := strings.Join(targetSlice, ",")
+	body := fmt.Sprintf("{\"mailer_target\": \"%v\",\"mailer_subject\": \"%v\",\"mailer_title\": \"%v\",\"mailer_content\": \"%v\"}", target, subject, title, content)
+	resp, err := http.Post(api, "application/json", strings.NewReader(body))
+	checkErr(err)
+	defer resp.Body.Close()
+	body1, err := ioutil.ReadAll(resp.Body)
+	checkErr(err)
+	fmt.Println(string(body1))
+}
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
